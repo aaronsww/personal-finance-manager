a2572494cc7236232f2c04492522ced625dc2e93
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-29 21:51:45 +0530

    style: set image size

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Users from "./Users";
 import { Link } from "react-router-dom";
-
 function Card({ username }) {
   console.log(username);
 
@@ -9,7 +8,7 @@ function Card({ username }) {
   return (
     <Link to="/chat">
       <div className="flex flex-col items-center ml-10">
-        <img className="rounded-full" src="" alt="big image"></img>
+        <img className="rounded-full h-40 w-40" src="" alt="big image"></img>
         <h2 className="font-bold text-xl">{username}</h2>{" "}
       </div>
     </Link>
