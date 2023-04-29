1692bdac2d77e5296bdbe81a8c90d31e71792228
f <franklinrohan2002@gmail.com>
2023-04-29 19:25:07 +0530

    chore: referenced register and login routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
       </Routes>
     </BrowserRouter>
   );
