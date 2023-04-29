1c5dc9f24a51a094a8c2b2334b123a9c27ee0887
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-29 19:34:46 +0530

    chore:Created Home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Users from "./Users";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <nav class="flex justify-between items-center bg-white h-16 p-0 m-0 shadow-md">
+        <div class="text-xl font-bold">Logo</div>
+
+        <div class="flex items-center">
+          <button class=" text-black font-serif rounded-md px-4 py-2 mr-4">
+            Profile
+          </button>
+          <button class=" text-black font-serif rounded-md px-4 py-2 mr-4">
+            Credit
+          </button>
+          <button class="text-black font-serif rounded-md px-4 py-2">
+            Debit
+          </button>
+        </div>
+      </nav>
+      <Users />
+    </div>
+  );
 }
 
 export default Home;
