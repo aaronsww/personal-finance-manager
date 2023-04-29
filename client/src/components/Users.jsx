eaf7ebf2e27a4a29fc908c3fb111af3c32d50f3f
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-29 22:40:52 +0530

    feat: Added Search Feature

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -6,6 +6,7 @@ function Users() {
   // const [users, setUsers] = useState(["Faiz", "Jeevan", "Trevis"]);
   const [users, setUsers] = useState([]);
   const [show, setShow] = useState();
+  const [searchUser, setSearchUser] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:5000/user/search").then((res) => {
@@ -15,7 +16,13 @@ function Users() {
     });
   }, []);
 
-  // console.log(users);
+  const perfromSearch = (user) => {
+    axios.get(`http://localhost:5000/user/search?name=${user}`).then((res) => {
+      console.log(res.data);
+      setUsers(res.data);
+      setShow(true);
+    });
+  };
 
   return (
     <div className="h-40 bg-sky-100 p-4">
@@ -25,9 +32,12 @@ function Users() {
         </h1>
         <input
           type="text"
+          value={searchUser}
+          onChange={(e) => setSearchUser(e.target.value)}
           placeholder="Search"
           className="bg-white-200 border-none focus:outline-none rounded-md py-1 px-2"
         />
+        <button onClick={() => perfromSearch(searchUser)}>Search</button>
       </div>
 
       <div className="flex flex-wrap mt-4">
