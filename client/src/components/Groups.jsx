26d784eb63dd1a2bb37c3c6ef21d87b8631db03a
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-30 01:13:59 +0530

    chore:Created Groups component

---
+++ client/src/components/Groups.jsx
@@ -0,0 +1,17 @@
+import CardGroup from "./CardGroup";
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      <div className="py  -10 bg-sky-100 p-4">
+        <div className="flex justify-between items-center">
+          <p className="text-start text-black text-3xl ml-12 mb-6 font-bold ">
+            Groups
+          </p>
+          <CardGroup />
+        </div>
+      </div>
+    </div>
+  );
+}
