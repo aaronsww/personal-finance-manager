26b010820c0f30d92df067ccfd3e3a8b1452fb70
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-29 19:52:54 +0530

    chore:References Chat component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Users from "./components/Users";
 import Card from "./components/Card";
+import Chat from "./components/Chat";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/users" element={<Users />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
       </Routes>
     </BrowserRouter>
   );
