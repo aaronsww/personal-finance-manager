5a04f480452060135638f12a62470e7d7ede4030
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-29 19:34:03 +0530

    chore:Referenced User component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Users from "./components/Users";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/users" element={<Users />}></Route>
       </Routes>
     </BrowserRouter>
   );
