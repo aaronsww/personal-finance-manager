604cdc421367c0145156824d086a1b711151ebfc
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-30 08:20:29 +0530

    feat: able to add debtor

--- client/src/components/Debtor.jsx
+++ client/src/components/Debtor.jsx
@@ -1,18 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { AuthContext } from "./AuthProvider";
 
 export default function () {
   const [searchUser, setSearchUser] = useState("");
+  const { user } = useContext(AuthContext);
+
+  const [amount, setAmount] = useState(0);
 
   const [show, setShow] = useState();
-  const [users, setUsers] = useState([{}]);
+  const [debtor, setDebtor] = useState({});
   const perfromSearch = (user) => {
-    axios.get(`http://localhost:5000/user/search?name=${user}`).then((res) => {
-      console.log(res.data);
-      setUsers([...users, { id: res.data[0]._id, name: res.data[0].name }]);
-      setShow(true);
-    });
+    axios
+      .get(`http://localhost:5000/user/search?name=${searchUser}`)
+      .then((res) => {
+        console.log(res.data);
+        setDebtor({ id: res.data[0]._id, name: res.data[0].name });
+        setShow(true);
+      });
   };
+
+  const handleClick = async (amount) => {
+    const response = await axios.post(
+      "http://localhost:5000/me/debtors/add",
+      { debtorId: debtor.id, amount: amount },
+      {
+        headers: { Authorization: `Bearer ${user.token}` },
+      }
+    );
+  };
+
   return (
     <div>
       <button>+ Debtors</button>
@@ -24,14 +41,19 @@ export default function () {
         className="bg-white-200 border-none focus:outline-none rounded-md py-1 px-2"
       />
       <button onClick={() => perfromSearch(searchUser)}>Search</button>
-      {/* <div>
-        <img
-          className="rounded-full h-32 w-32"
-          src={GroupCard}
-          alt="big image"
-        ></img>
-        <p className="font-medium text-sm">read?</p>
-      </div> */}
+      {show && (
+        <div>
+          <input
+            type="text"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Amount Owed"
+            className="bg-white-200 border-none focus:outline-none rounded-md py-1 px-2"
+          />
+          {debtor.name}
+          <button onClick={() => handleClick(amount)}>Submit</button>
+        </div>
+      )}
     </div>
   );
 }
