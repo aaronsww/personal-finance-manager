6038f84768b085aa3cd163852cb259a92f543dbe
faiz887 <faiz.20201CSD0115@presidencyuniversity.in>
2023-04-30 08:45:42 +0530

    feat: fetches balance and updates it

--- client/src/components/Wallet.jsx
+++ client/src/components/Wallet.jsx
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, useState } from "react";
 import walletImg from "../images/wallet.png";
 import axios from "axios";
 import { AuthContext } from "./AuthProvider";
+import { Link, useNavigate } from "react-router-dom";
 
 function Wallet() {
   const [savings, setSavings] = useState(0);
   const [show, setShow] = useState();
+  const navigate = useNavigate();
 
   const { user } = useContext(AuthContext);
 
@@ -17,21 +19,29 @@ function Wallet() {
   useEffect(() => {
     (async () => {
       console.log(user.token);
-      const response = await axios.get("http://localhost:5000/me", {
+      const response = await axios.get("http://localhost:5000/me/balance", {
         headers: { Authorization: `Bearer ${user.token}` },
       });
-      console.log(response);
+      setSavings(response.data.balance);
     })();
   }, []);
 
-  const handleClick = async () => {};
+  const handleSave = async (savings) => {
+    const response = await axios.patch(
+      "http://localhost:5000/me/balance",
+      { balance: savings },
+      {
+        headers: { Authorization: `Bearer ${user.token}` },
+      }
+    );
+    if (response.status === 200) navigate("/");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-blue-100">
       <div className="bg-white shadow-lg rounded-lg p-8">
         <h1 className="text-2xl font-bold mb-4">My Wallet</h1>
         <img src={walletImg} alt="wallet" className="ml-12 h-40 w-40 mb-4" />
-
         <div className="flex items-center border-b-2 border-gray-400 py-2 mb-4">
           <label className="mr-4 font-bold">Current Savings:</label>
           <input
@@ -40,6 +50,7 @@ function Wallet() {
             onChange={handleSavingsChange}
             className="border border-gray-400 py-2 px-4 rounded-lg w-32"
           />
+          {show && <div onClick={() => handleSave(savings)}>Save</div>}
         </div>
       </div>
     </div>
